f342738218d3014fbff02c4dab1543b75b527d63
Abdullah Tahir <ababdullah216@gmail.com>
2025-10-10 14:47:10 +0500

    project setup completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-5xl bg-amber-400 xl:text-amber-700 md:text-blue-800 ">
+        hello
+      </p>
+    </div>
+  );
+};
+
+export default App;
